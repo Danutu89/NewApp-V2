f6b6eba1362869afacb9ef545daf02ffc2e4cd3f
Danutu89 <danutu@newapp.nl>
2020-06-07 15:04:12 +0200

    registration fixed

--- client/src/modules/Auth/register.js
+++ client/src/modules/Auth/register.js
@@ -2,7 +2,7 @@ import {instance} from '../Requests.js';
 import {api as Api, get} from '../../modules/Store';
 
 export default async(payload)=>{
-    const res = await instance.post(get(Api)['users.register'] ,payload).then(function (response) {
+    const res = await instance.post(get(Api)['auth.register'] ,payload).then(function (response) {
         return response.data;
     }).then(function (response){
         return response;
